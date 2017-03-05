cbab859bc23b8e3b9a04529624484eb85d88cc28
Li Chenxi <3059846899@qq.com>
2017-03-06 00:17:23 +0800

    完善url设计

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import apis
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<blog_id>[0-9]+)/$', views.get_blog, name='blog'),
-    url(r'^getList/$', apis.api_blog_list, name='blog_list'),
-    url(r'^api/(?P<blog_id>[0-9]+)/comments/$', apis.api_blog_comments, name='blog_comments'),
+    url(r'^blog/(?P<blog_id>[0-9]+)/$', views.get_blog, name='blog'),
+    url(r'^blog/getList/$', apis.api_blog_list, name='blog_list'),
+    url(r'^blog/api/(?P<blog_id>[0-9]+)/comments/$', apis.api_blog_comments, name='blog_comments'),
     url(r'^(?P<comment_id>[0-9]+)/comments/delete/$', apis.api_comments_delete, name='comments_delete')
 ]
