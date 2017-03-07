bbd460f67a083931f08804eb6dbe796d001e0e99
Li Chenxi <3059846899@qq.com>
2017-03-07 23:05:48 +0800

    完成部署

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from . import apis
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^blog/$', views.index, name='index'),
     url(r'^blog/(?P<blog_id>[0-9]+)/$', views.get_blog, name='blog'),
     url(r'^blog/getList/$', apis.api_blog_list, name='blog_list'),
     url(r'^blog/api/(?P<blog_id>[0-9]+)/comments/$', apis.api_blog_comments, name='blog_comments'),
-    url(r'^(?P<comment_id>[0-9]+)/comments/delete/$', apis.api_comments_delete, name='comments_delete')
+    url(r'^blog/(?P<comment_id>[0-9]+)/comments/delete/$', apis.api_comments_delete, name='comments_delete')
 ]
